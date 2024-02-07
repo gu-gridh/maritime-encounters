1bebcedaa934df8a4b2b62fe0f01bdd9d0a573df
AramKarimi <aram.karimi@gu.se>
2024-02-07 11:10:59 +0100

    update requirement file

--- maritime/maritime/settings.py
+++ maritime/maritime/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from django.utils.translation import gettext_lazy as _
+# from diana.utils import read_json
+# from .settings_local import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,8 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-tf+@m0ju)eh5_#w)p8y(w662x%%)l+o)af9pn!$6^e14hae89&"
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
