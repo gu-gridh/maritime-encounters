c1e8214e9efd034df703f5f90cb9a2b9ca1d9c2b
ashely-ra <ashely.green@gu.se>
2024-07-11 22:51:55 +0200

    fix typos

--- Maritime-Backend/upload_scripts/import-metalwork.py
+++ Maritime-Backend/upload_scripts/import-metalwork.py
@@ -21,27 +21,27 @@ django.setup()
 from apps.resources.models import *
 from apps.geography.models import ADM0, ADM1, ADM2, ADM3, ADM4, ADM5, Province, Parish
 
-Site.objects.all().delete()
-Location.objects.all().delete()
-Metalwork.objects.all().delete()
-ContextKeywords.objects.all().delete()
-Period.objects.all().delete()
-ObjectCounts.objects.all().delete()
-ObjectMaterials.objects.all().delete()
-ObjectDescription.objects.all().delete()
-ObjectSubcategories.objects.all().delete()
-ObjectCategories.objects.all().delete()
-ContextDetail.objects.all().delete()
-ContextFindsCategories.objects.all().delete()
-ContextFindsSubcategories.objects.all().delete()
+# Site.objects.all().delete()
+# Location.objects.all().delete()
+# Metalwork.objects.all().delete()
+# ContextKeywords.objects.all().delete()
+# Period.objects.all().delete()
+# ObjectCounts.objects.all().delete()
+# ObjectMaterials.objects.all().delete()
+# ObjectDescription.objects.all().delete()
+# ObjectSubcategories.objects.all().delete()
+# ObjectCategories.objects.all().delete()
+# ContextDetail.objects.all().delete()
+# ContextFindsCategories.objects.all().delete()
+# ContextFindsSubcategories.objects.all().delete()
 
 
 csv_file_path = '/mnt/k/conv_metal_db/c_horn_metalwork_v2.csv'
 
 # Load the CSV data
 df = pd.read_csv(csv_file_path)
-df_clip = df[:2000]
-df_clip = df_clip.replace(np.nan, None).replace('[]',None)
+# df_clip = df[:2000]
+# df_clip = df_clip.replace(np.nan, None).replace('[]',None)
 # Import data into caches
 site_cache = {}
 entry_num_cache = {}
@@ -54,7 +54,7 @@ finds_cache = {}
 context_detail_cache = {}
 
 
-for place, adm0n, adm1n, adm2n, adm3n, adm4n, provincen, parishn, x, y in df_clip[['place', 'ADM_0', 'ADM_1', 'ADM_2', 'ADM_3', 'ADM_4', 'province', 'parish', 'x', 'y']].drop_duplicates(['x','y','place']).values:
+for place, adm0n, adm1n, adm2n, adm3n, adm4n, provincen, parishn, x, y in df[['place', 'ADM_0', 'ADM_1', 'ADM_2', 'ADM_3', 'ADM_4', 'province', 'parish', 'x', 'y']].drop_duplicates(['x','y','place']).values:
     site_name = place or f"{parish}: {y}, {x}" or f"{province}: {y}, {x}" or f"{adm4n}: {y}, {x}" or f"{adm3n}: {y}, {x}" or f"{adm2n}: {y}, {x}"
     adm0 = ADM0.objects.get(name=adm0n) if adm0n != None else None
     adm1 = ADM1.objects.get(name=adm1n) if adm1n != None else None
@@ -79,11 +79,11 @@ for place, adm0n, adm1n, adm2n, adm3n, adm4n, provincen, parishn, x, y in df_cli
     site_cache[x] = site
     
 
-desc_df = pd.json_normalize(literal_eval(df_clip['certainContextDescriptors'][0]))
+desc_df = pd.json_normalize(literal_eval(df['certainContextDescriptors'][0]))
 for category in desc_df.columns:
     ContextFindsCategories.objects.get_or_create(text=category.capitalize())
 
-for row in df_clip.itertuples(index=False):
+for row in df.itertuples(index=False):
     site_name = row.place or f"{row.parish}: {row.y}, {row.x}" or f"{row.province}: {row.y}, {row.x}" or f"{row.ADM_4}: {row.y}, {row.x}" or f"{row.ADM_3}: {row.y}, {row.x}" or f"{row.ADM_2}: {row.y}, {row.x}"
     
 
