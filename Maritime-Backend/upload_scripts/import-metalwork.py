edc4abb950b3d67dda582e389fea6e13138bae88
AramKarimi <aram.karimi@gu.se>
2024-08-26 13:41:29 +0200

    new scipt

--- Maritime-Backend/upload_scripts/import-metalwork.py
+++ Maritime-Backend/upload_scripts/import-metalwork.py
@@ -18,55 +18,38 @@ django.setup()
 from apps.geography.models import ADM0, ADM1, ADM2, ADM3, ADM4, Province, Parish
 from apps.resources.models import *
 
-
-# Clear current data in database
-# Site.objects.all().delete()
-# Location.objects.all().delete()
-# Metalwork.objects.all().delete()
-# ContextKeywords.objects.all().delete()
-# Period.objects.all().delete()
-# ObjectCount.objects.all().delete()
-# ObjectMaterials.objects.all().delete()
-# ObjectDescription.objects.all().delete()
-# ObjectSubcategories.objects.all().delete()
-# ObjectCategories.objects.all().delete()
-# ContextDetail.objects.all().delete()
-# ContextFindsCategories.objects.all().delete()
-# ContextFindsSubcategories.objects.all().delete()
-
-
 # Load the CSV data
-
 def import_metalwork(csv_file_path):
     df = pd.read_csv(csv_file_path).replace(np.nan, None).replace('[]', None)
 
+    # Normalize descriptors to avoid multiple evaluations in the loop
+    desc_df = pd.json_normalize(literal_eval(df['certainContextDescriptors'][0]))
+    for category in desc_df.columns:
+        ContextFindsCategories.objects.get_or_create(text=category.capitalize())
 
-    # Add administrative data to sites and create site objects
-    for place, adm0n, adm1n, adm2n, adm3n, adm4n, provincen, parishn, x, y in df[['place', 'ADM_0', 'ADM_1', 'ADM_2', 'ADM_3', 'ADM_4', 'province', 'parish', 'x', 'y']].drop_duplicates(['x', 'y', 'place']).values:
-        # Note that Point takes (longitude, latitude) order
+    for row in df.itertuples(index=False):
+        # Handle site creation
+        x, y = row.x, row.y
         point = Point(x, y) if not pd.isnull(y) or pd.isnull(x) else None
-        
-        site_name = place or f"{parishn}: {y}, {x}" or f"{provincen}: {y}, {x}" or f"{adm4n}: {y}, {x}" or f"{adm3n}: {y}, {x}" or f"{adm2n}: {y}, {x}"
-                
-        adm0 = ADM0.objects.get(name=adm0n) if adm0n != None else None
-        adm1 = ADM1.objects.get(name=adm1n) if adm1n != None else None
-        adm2 = ADM2.objects.get(
-            name=adm2n, ADM1__name=adm1n) if adm2n != None else None
-        adm3 = ADM3.objects.get(
-            name=adm3n, ADM2__name=adm2n) if adm3n != None else None
-        adm4 = ADM4.objects.get(name=adm4n, ADM3__name=adm3n,
-                                ADM3__ADM2__name=adm2n) if adm4n != None else None
+        site_name = row.place or f"{row.parish}: {y}, {x}" or f"{row.province}: {y}, {x}" or f"{row.ADM_4}: {y}, {x}" or f"{row.ADM_3}: {y}, {x}" or f"{row.ADM_2}: {y}, {x}"
+
+        adm0 = ADM0.objects.get(name=row.ADM_0) if row.ADM_0 else None
+        adm1 = ADM1.objects.get(name=row.ADM_1) if row.ADM_1 else None
+        adm2 = ADM2.objects.get(name=row.ADM_2, ADM1__name=row.ADM_1) if row.ADM_2 else None
+        adm3 = ADM3.objects.get(name=row.ADM_3, ADM2__name=row.ADM_2) if row.ADM_3 else None
+        adm4 = ADM4.objects.get(name=row.ADM_4, ADM3__name=row.ADM_3, ADM3__ADM2__name=row.ADM_2) if row.ADM_4 else None
+        province, parish = None, None
+
         try:
             province = Province.objects.get(geometry__contains=point)
         except:
-            province = None
+            pass
         try:
             parish = Parish.objects.get(geometry__contains=point)
         except:
-            parish = None
-        
+            pass
 
-        Site.objects.get_or_create(
+        site_obj, _ = Site.objects.get_or_create(
             name=site_name,
             ADM0=adm0,
             ADM1=adm1,
@@ -78,175 +61,118 @@ def import_metalwork(csv_file_path):
             coordinates=point
         )
 
-    # Add finds categories to database
-    desc_df = pd.json_normalize(literal_eval(df['certainContextDescriptors'][0]))
-    for category in desc_df.columns:
-        ContextFindsCategories.objects.get_or_create(text=category.capitalize())
+        # Process Metalwork data for each row
+        multiperiod_bool = bool(row.dating) and len(literal_eval(row.dating)) > 1
 
-    # Import data by row
-    for row in df.itertuples(index=False):
-        site_name = row.place or f"{row.parish}: {row.y}, {row.x}" or f"{row.province}: {row.y}, {row.x}" or f"{row.ADM_4}: {row.y}, {row.x}" or f"{row.ADM_3}: {row.y}, {row.x}" or f"{row.ADM_2}: {row.y}, {row.x}" or "NAME IS MISSING"
-        if row.dating != None:
-            multiperiod_bool = False if len(literal_eval(row.dating)) <1 else True
-        else:
-            multiperiod_bool = False
-        site_row =Site.objects.get_or_create(coordinates=Point(row.x, row.y) if row.x and row.y else None, name=site_name)[0]
-        print(site_row)
-        # Add data to boolean, character/text, and some foreignkey fields
-        # db_object = Metalwork.objects.update_or_create(
-        #     entry_num=EntryNum.objects.get_or_create(entry_number=row.entryNo)[0],
-        #     literature_num=LiteratureNum.objects.get_or_create(
-        #         literature_number=row.literatureNo)[0],
-        #     accession_num=AccessionNum.objects.get_or_create(
-        #         accession_number=row.accessionNo)[0],
-        #     accession_certain=row.accessionCertain,
-        #     museum_certain=row.museumCertain,
-        #     location=Location.objects.get_or_create(
-        #         site=site_row, location_detail=row.placeDetail)[0],
-        #     location_certain=row.locationCertain,
-        #     coord_system=row.origCoordSys,
-        #     orig_coords=[row.xOrig, row.yOrig] if row.xOrig or row.yOrig else None,
-        #     main_context=Context.objects.get_or_create(
-        #         text=row.mainContext if row.mainContext != None else 'Uncertain')[0],
-        #     main_context_certain=row.mainContextCertain,
-        #     find_context=FindContext.objects.get_or_create(
-        #         text=row.findContext)[0],
-        #     find_context_certain=row.findContextCertain,
-        #     context_detail=ContextDetail.objects.get_or_create(
-        #         text=row.detailContext if row.detailContext != None else 'Unknown')[0],
-        #     context_detail_certain=row.detailContextCertain,
-        #     # Inverted boolean value to handle mixup during data export
-        #     multiperiod=multiperiod_bool,
-        #     date_string=row.datingString,
-        #     dating_certain=row.datingCertain,
-        #     dendro_date=row.dendroDate,
-        #     radiocarbon_date=row.radioCarbonDate,
-        #     radiocarbon_years=row.radioCarbonYear,
-        #     radiocarbon_std=row.stdDeviation,
-        #     comments=row.comments
-        # )[0]
+        db_object, _ = Metalwork.objects.update_or_create(
+            entry_num=EntryNum.objects.get_or_create(entry_number=row.entryNo)[0],
+            literature_num=LiteratureNum.objects.get_or_create(literature_number=row.literatureNo)[0],
+            accession_num=AccessionNum.objects.get_or_create(accession_number=row.accessionNo)[0],
+            defaults={
+                'accession_certain': row.accessionCertain,
+                'museum_certain': row.museumCertain,
+                'location': Location.objects.get_or_create(site=site_obj, location_detail=row.placeDetail)[0],
+                'location_certain': row.locationCertain,
+                'coord_system': row.origCoordSys,
+                'orig_coords': [row.xOrig, row.yOrig] if row.xOrig or row.yOrig else None,
+                'main_context': Context.objects.get_or_create(text=row.mainContext if row.mainContext else 'Uncertain')[0],
+                'main_context_certain': row.mainContextCertain,
+                'find_context': FindContext.objects.get_or_create(text=row.findContext)[0],
+                'find_context_certain': row.findContextCertain,
+                'context_detail': ContextDetail.objects.get_or_create(text=row.detailContext if row.detailContext else 'Unknown')[0],
+                'context_detail_certain': row.detailContextCertain,
+                'multiperiod': multiperiod_bool,
+                'date_string': row.datingString,
+                'dating_certain': row.datingCertain,
+                'dendro_date': row.dendroDate,
+                'radiocarbon_date': row.radioCarbonDate,
+                'radiocarbon_years': row.radioCarbonYear,
+                'radiocarbon_std': row.stdDeviation,
+                'comments': row.comments
+            }
+        )
 
-        # # Add data to remaining fields with more complex data structure
-        # keywords = []
-        # datings = []
-        # objects_list = []
-        # cert_context_desc = []
-        # poss_context_desc = []
+        # Process complex fields
+        keywords, datings, objects_list = [], [], []
+        cert_context_desc, poss_context_desc = [], []
 
-        # # Add objects for all keywords to list
-        # if row.contextKeywords != None:
-        #     for desc in literal_eval(row.contextKeywords):
-        #         desc_text = ContextKeywords.objects.get_or_create(text=desc)[0]
-        #         keywords.append(desc_text)
+        if row.contextKeywords:
+            for desc in literal_eval(row.contextKeywords):
+                keywords.append(ContextKeywords.objects.get_or_create(text=desc)[0])
 
-        # # Separate phase values from periods and add objects to list
-        # if row.dating != None:
-        #     for dating in literal_eval(row.dating):
-        #         if 'PI' or 'PV' or 'BA' in dating:
-        #             phase_text = Phase.objects.get_or_create(text=dating)[0]
-        #             dating_text = Period.objects.get_or_create(
-        #                 name='Bronze Age', phase=phase_text)[0]
-        #             datings.append(dating_text)
-        #         elif 'IA' in dating:
-        #             phase_text = Phase.objects.get_or_create(text=dating)[0]
-        #             dating_text = Period.objects.get_or_create(
-        #                 name='Iron Age', phase=phase_text)[0]
-        #             datings.append(dating_text)
-        #         elif ('Early' or 'Late' or 'Middle' in dating) and 'Neolithic' in dating:
-        #             phase_text = Phase.objects.get_or_create(text=dating)[0]
-        #             dating_text = Period.objects.get_or_create(
-        #                 name='Neolithic', phase=phase_text)[0]
-        #             datings.append(dating_text)
-        #         elif ('ERT' or 'RT' in dating):
-        #             phase_text = Phase.objects.get_or_create(text=dating)[0]
-        #             dating_text = Period.objects.get_or_create(name='Iron Age', phase = phase_text)
-        #             datings.append(dating_text)
-        #         elif 'EGK' in dating:
-        #             phase_text = Phase.objects.get_or_create(text=dating)[0]
-        #             dating_text = Period.objects.get_or_create(name='Neolithic', phase = phase_text)
-        #             datings.append(dating_text)
-        #         else:
-        #             dating_text = Period.objects.get_or_create(name=dating)[0]
-        #             datings.append(dating_text)
+        if row.dating:
+            for dating in literal_eval(row.dating):
+                if 'PI' or 'PV' or 'BA' in dating:
+                    phase = Phase.objects.get_or_create(text=dating)[0]
+                    datings.append(Period.objects.get_or_create(name='Bronze Age', phase=phase)[0])
+                elif 'IA' in dating:
+                    phase = Phase.objects.get_or_create(text=dating)[0]
+                    datings.append(Period.objects.get_or_create(name='Iron Age', phase=phase)[0])
+                elif 'Neolithic' in dating:
+                    phase = Phase.objects.get_or_create(text=dating)[0]
+                    datings.append(Period.objects.get_or_create(name='Neolithic', phase=phase)[0])
+                elif 'ERT' or 'RT' in dating:
+                    phase = Phase.objects.get_or_create(text=dating)[0]
+                    datings.append(Period.objects.get_or_create(name='Iron Age', phase=phase)[0])
+                elif 'EGK' in dating:
+                    phase = Phase.objects.get_or_create(text=dating)[0]
+                    datings.append(Period.objects.get_or_create(name='Neolithic', phase=phase)[0])
+                else:
+                    datings.append(Period.objects.get_or_create(name=dating)[0])
 
-        # # Create finds categories and subcategories, edit material values for normalisation, generate a list of material objects, add the object and count data to the current metalwork object
-        # if row.findsOverview != None:
-        #     data = pd.json_normalize(literal_eval(row.findsOverview))
-        #     data.rename(columns={'count': 'obj_count'}, inplace=True)
-        #     for finds in data.itertuples():
-        #         category_text = ObjectCategories.objects.get_or_create(
-        #             text=finds.category.capitalize())[0]
-        #         subcategory_text = ObjectSubcategories.objects.get_or_create(
-        #             subcategory=finds.subcategory.capitalize())[0]
-        #         materials = []
-        #         category_list = []
-        #         for material in finds.material:
-        #             if material.capitalize().strip() == 'Au':
-        #                 material = 'Gold'
-        #             if material.capitalize().strip() == 'Cu':
-        #                 material = 'Copper'
-        #             if material.capitalize().strip() == 'Fe':
-        #                 material = 'Iron'
-        #             if material.capitalize().strip() == 'Sn':
-        #                 material = 'Tin'
-        #             if material.capitalize().strip() == 'Ag':
-        #                 material = 'Silver'
-        #             if material.capitalize().strip() == 'Bz':
-        #                 material = 'Bronze'
-        #             if material.capitalize().strip() == 'Kn':
-        #                 material = 'Bone'
-        #             material_text = ObjectMaterials.objects.get_or_create(
-        #                 text=material.capitalize())[0]
-        #             materials.append(material_text)
+        if row.findsOverview:
+            data = pd.json_normalize(literal_eval(row.findsOverview))
+            data.rename(columns={'count': 'obj_count'}, inplace=True)
+            for finds in data.itertuples():
+                category = ObjectCategories.objects.get_or_create(text=finds.category.capitalize())[0]
+                subcategory = ObjectSubcategories.objects.get_or_create(subcategory=finds.subcategory.capitalize())[0]
+                materials = [
+                    ObjectMaterials.objects.get_or_create(
+                        text=material.capitalize().strip()
+                        .replace('Au', 'Gold')
+                        .replace('Cu', 'Copper')
+                        .replace('Fe', 'Iron')
+                        .replace('Sn', 'Tin')
+                        .replace('Ag', 'Silver')
+                        .replace('Bz', 'Bronze')
+                        .replace('Kn', 'Bone')
+                    )[0]
+                    for material in finds.material
+                ]
 
-        #         category_text = ObjectCategories.objects.get_or_create(text='Weapons')[0]
-        #         category_list.append(category_text)
-     
-        #         object_desc = ObjectDescription.objects.get_or_create(
-        #             subcategory=subcategory_text)[0]
-        #         object_desc.category.set(category_list) 
-                
+                object_desc = ObjectDescription.objects.get_or_create(subcategory=subcategory)[0]
+                object_desc.category.set([category])
+                object_counts = ObjectCount.objects.create(
+                    metal=db_object, object=object_desc, count=finds.obj_count, certainty=finds.certain)
+                object_counts.material.set(materials)
+                objects_list.append(object_counts)
 
-        #         # db_object = Metalwork.objects.get(entry_num__entry_number=row.entryNo, literature_num__literature_number=row.literatureNo)
-        #         object_counts = ObjectCount.objects.create(
-        #             metal=db_object, object=object_desc, count=finds.obj_count, certainty=finds.certain)
-        #         object_counts.material.set(materials)
-        #         objects_list.append(object_counts)
+        if row.certainContextDescriptors:
+            data = pd.json_normalize(literal_eval(row.certainContextDescriptors))
+            for category in data.columns:
+                category_obj = ContextFindsCategories.objects.get(text=category.capitalize())
+                for subcat in data[category].values[0]:
+                    cert_context_desc.append(ContextFindsSubcategories.objects.get_or_create(
+                        text=subcat.capitalize(), category=category_obj)[0])
 
-        # # Assign categories to the subcategories using keys in the dictionary, append the subcategory objects to a list, add the list to the current db object
-        # data = pd.json_normalize(literal_eval(row.certainContextDescriptors))
-        # for category in data.columns:
-        #     category_obj = ContextFindsCategories.objects.get(
-        #         text=category.capitalize())
-        #     for subcat in data[category].values[0]:
-        #         context_subcat = ContextFindsSubcategories.objects.get_or_create(
-        #             text=subcat.capitalize(), category=category_obj)[0]
-        #         cert_context_desc.append(context_subcat)
+        if row.uncertainContextDescriptors:
+            data = pd.json_normalize(literal_eval(row.uncertainContextDescriptors))
+            for category in data.columns:
+                category_obj = ContextFindsCategories.objects.get(text=category.capitalize())
+                for subcat in data[category].values[0]:
+                    poss_context_desc.append(ContextFindsSubcategories.objects.get_or_create(
+                        text=subcat.capitalize(), category=category_obj)[0])
 
-        # data = pd.json_normalize(literal_eval(row.uncertainContextDescriptors))
-        # for category in data.columns:
-        #     category_obj = ContextFindsCategories.objects.get(
-        #         text=category.capitalize())
-        #     for subcat in data[category].values[0]:
-        #         context_subcat = ContextFindsSubcategories.objects.get_or_create(
-        #             text=subcat.capitalize(), category=category_obj)[0]
-        #         poss_context_desc.append(context_subcat)
-                
-        # if row.museumCollection != None:
-        #     collections_list=[]
-        #     for collection in row.museumCollection.title().split(';'):        
-        #         mus_collect=MuseumCollection.objects.get_or_create(collection=collection.strip())[
-        #                 0]
-        #         collections_list.append(mus_collect)
+        if row.museumCollection:
+            collections_list = [MuseumCollection.objects.get_or_create(collection=collection.strip())[0]
+                                for collection in row.museumCollection.title().split(';')]
 
-        # # Set ManyToMany fields using the lists
-        # # db_object = Metalwork.objects.get(entry_num__entry_number=row.entryNo, literature_num__literature_number=row.literatureNo)
-        # db_object.context_keywords.set(keywords)
-        # db_object.dating.set(datings)
-        # db_object.certain_context_descriptors.set(cert_context_desc)
-        # db_object.uncertain_context_descriptors.set(poss_context_desc)
-        # db_object.collection.set(collections_list)
-
+        # Set ManyToMany fields
+        db_object.context_keywords.set(keywords)
+        db_object.dating.set(datings)
+        db_object.certain_context_descriptors.set(cert_context_desc)
+        db_object.uncertain_context_descriptors.set(poss_context_desc)
+        db_object.collection.set(collections_list)
 
     print("Data imported successfully")
     
