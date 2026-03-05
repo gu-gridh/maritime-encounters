5336744c4ef333552a4a0b06ce167b28b66b668c
AramKarimi <aram.karimi@gu.se>
2026-03-05 15:12:47 +0100

    fixed location error

--- Maritime-Backend/apps/resources/models.py
+++ Maritime-Backend/apps/resources/models.py
@@ -21,7 +21,7 @@ class Location(abstract.AbstractBaseModel):
     #     "Coordinates"), help_text=_("Mid-point coordinates of the location."))
 
     def __str__(self) -> str:
-        return self.location_name or self.location_detail or self.site.name or ''
+        return self.location_name or self.location_detail  or ''
 
     class Meta:
         verbose_name = _("Location")
